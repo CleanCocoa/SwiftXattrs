6a4a8fe762a28deed09ed8b5e405112f58f4844c
Christian Tietze <christian.tietze@gmail.com>
2017-09-16 08:08:28 +0200

    Initial Commit

---
+++ SwiftXattrs/SwiftXattrs.h
@@ -0,0 +1,19 @@
+//
+//  SwiftXattrs.h
+//  SwiftXattrs
+//
+//  Created by Christian Tietze on 16.09.17.
+//  Copyright © 2017 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftXattrs.
+FOUNDATION_EXPORT double SwiftXattrsVersionNumber;
+
+//! Project version string for SwiftXattrs.
+FOUNDATION_EXPORT const unsigned char SwiftXattrsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftXattrs/PublicHeader.h>
+
+
